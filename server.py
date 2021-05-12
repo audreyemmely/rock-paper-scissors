9a9f497c7a050e97597ad076682949c780432617
Audrey <aerv@ic.ufal.br>
2021-05-12 17:34:18 -0300

    Creating server.py

---
+++ server.py
@@ -0,0 +1,21 @@
+import socket
+from _thread import *
+import pickle
+from game import Game
+
+server = "type_your_ip_address_here"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+s.listen(2)
+print("Waiting for a connection, Server Started")
+
+connected = set()
+games = {}
+idCount = 0
