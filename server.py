fb3c4bfa113a4dd1fddd64ed6f78519f7512b98c
Audrey <aerv@ic.ufal.br>
2021-05-12 21:42:36 -0300

    creating threaded_client function

--- server.py
+++ server.py
@@ -19,3 +19,38 @@ print("Waiting for a connection, Server Started")
 connected = set()
 games = {}
 idCount = 0
+
+def threaded_client(conn, p, gameId):
+    global idCount
+    conn.send(str.encode(str(p)))
+
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(4096).decode()
+
+            if gameId in games:
+                game = games[gameId]
+
+                if not data:
+                    break
+                else:
+                    if data == "reset":
+                        game.resetWent()
+                    elif data != "get":
+                        game.play(p, data)
+
+                    conn.sendall(pickle.dumps(game))
+            else:
+                break
+        except:
+            break
+
+    print("Lost connection")
+    try:
+        del games[gameId]
+        print("Closing Game", gameId)
+    except:
+        pass
+    idCount -= 1
+    conn.close()
