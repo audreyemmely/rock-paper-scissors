ecd564faa2312ac0c246e402082f173ba60d8ee0
Audrey <aerv@ic.ufal.br>
2021-05-15 19:52:57 -0300

    creating game

---
+++ game.py
@@ -0,0 +1,9 @@
+class Game:
+    def __init__(self, id):
+        self.p1Went = False
+        self.p2Went = False
+        self.ready = False
+        self.id = id
+        self.moves = [None, None]
+        self.wins = [0,0]
+        self.ties = 0
